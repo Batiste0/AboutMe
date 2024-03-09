f8cfbf455a2b350a81b8af91e434c9c74a68ee34
Starfier <97109070+Starfier@users.noreply.github.com>
2024-03-09 01:10:26 +0100

    olelal casi finish le code pour toutes les pages ordinateur

--- electronique/computer/script.js
+++ electronique/computer/script.js
@@ -22,6 +22,66 @@ function gohome(){
     }
 }
 
+var actualdisplayer = 0; // 0 = img, 1 = video
+var videodir = "../../Videos/electronique/";
+var imgdir = "../../Images/electronique/";
+
+async function DisplayVideo(id){
+return new Promise(r => {
+if(actualdisplayer === 0){
+var oldel = document.getElementById("displayer");
+var newVideoEL = document.createElement("video");
+newVideoEL.muted = true;
+newVideoEL.autoplay = true;
+newVideoEL.id = "displayer";
+newVideoEL.alt = "illustration";
+newVideoEL.src = videodir + String(id) + ".mp4";
+newVideoEL.removeAttribute("controls");
+oldel.parentNode.replaceChild(newVideoEL, oldel);
+actualdisplayer = 1;
+newVideoEL.addEventListener("play", function() {
+    return r();
+});
+}else{
+var oldel = document.getElementById("displayer");
+oldel.muted = true;
+oldel.autoplay = true;
+oldel.id = "displayer";
+oldel.alt = "illustration";
+oldel.src = videodir + String(id) + ".mp4";
+oldel.addEventListener("play", function() {
+    return r();
+});
+}
+})
+}
+
+
+async function DisplayImage(id){
+return new Promise(r => {
+    if(actualdisplayer === 1){
+    var oldel = document.getElementById("displayer");
+    var newImgEL = document.createElement("img");
+    newImgEL.id = "displayer";
+    newImgEL.alt = "illustration";
+    newImgEL.src = imgdir + String(id) + ".jpg";
+    newImgEL.removeAttribute("controls");
+    oldel.parentNode.replaceChild(newImgEL, oldel);
+    newImgEL.addEventListener("load", function() {
+        return r();
+    });
+    }else{
+    var oldel = document.getElementById("displayer");
+    oldel.id = "displayer";
+    oldel.alt = "illustration";
+    oldel.src = imgdir + String(id) + ".jpg";
+    oldel.addEventListener("load", function() {
+        return r();
+    });
+    }
+    })
+}
+
 window.onload = function() {
     CheckRedirection();
     var video = document.getElementById('fondvideo');
@@ -29,5 +89,14 @@ window.onload = function() {
     video.oncanplay = function() {
       setTimeout(() => background.style.display = 'none',3500) 
     };
+    DisplayVideo(0).then(() => {
+        setTimeout(() => {
+            DisplayImage(0).then(() => {
+                setTimeout(() => {
+                    DisplayImage(1)
+                },2000)
+            })
+        },2000)
+    })
   };
   
